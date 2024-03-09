4a7bfe10559984e753f898b61363d72db23750c9
nikhilar98 <nikhilar98@gmail.com>
2024-03-09 13:20:41 +0530

    spinner update

--- app/components/UserForm.js
+++ app/components/UserForm.js
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import axios from "axios";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 const UserForm = () => { 
 
     const [userInput,setUserInput] = useState("")
@@ -10,9 +12,11 @@ const UserForm = () => {
     const [gender,setGender] = useState("")
     const [nationality,setNationality] = useState("")
     const [countryName,setCountryName] = useState("")
+    const [isLoading,setIsLoading] = useState(false)
     
     async function handleSubmit(e){
         e.preventDefault()
+        setIsLoading(true)
         try{
             const response = await Promise.all([await axios.get(` https://api.agify.io?name=${userInput}`),
             await axios.get(` https://api.genderize.io?name=${userInput}`),
@@ -24,6 +28,7 @@ const UserForm = () => {
             const maxProbablity = Math.max(...country.map(ele=>ele.probability))
             const finalCountry = country.find(country=>country.probability===maxProbablity)?.country_id
             const response2 = await axios.get(`https://restcountries.com/v3.1/alpha/${finalCountry}`)
+            setIsLoading(false)
             setAge(age)
             setGender(gender)
             setNationality(finalCountry)
@@ -31,6 +36,7 @@ const UserForm = () => {
             setUserInput("")
         }
         catch(err){
+            setIsLoading(false)
             toast.error(err.response.data?.error || err.response.data?.message)
         }
        
@@ -42,6 +48,12 @@ const UserForm = () => {
                 <input type="text" id="name" value={userInput} onChange={(e)=>{setUserInput(e.target.value)}} placeholder="Enter your name"/>
                 <input type="submit" value="submit" id="submit"/>
             </form>
+            {
+                isLoading &&   
+                <Box>
+                    <CircularProgress size={25} sx={{margin:'12px'}}/>
+                </Box>
+            }
             <div>
                 {Boolean(age) && <p>I think your age is {age}.</p>}
                 {gender && <p>Your gender is {gender}.</p>}
